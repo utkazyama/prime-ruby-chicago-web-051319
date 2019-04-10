1dfca50bfcbbee12b19f237848b2ba7389a042a5
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-10 22:00:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
 # Add  code here!
+def prime?(num)
+if num<0 || num==0 || num==1
+  return FALSE
+else 
+  (2..num-1).to.a_all? do |possible_factor|
+    num % possible_factor =! 0
+  end
+end
+end

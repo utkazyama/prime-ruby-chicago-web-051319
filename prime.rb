04c253c3199e976c9c0cb72433a77db791cce403
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-10 22:01:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,10 @@
+# Add  code here!
 def prime?(num)
-  if num < 0 or num == 0 or num == 1
-    return false
-  else
-    (2..num-1).to_a.all? do |possible_factor|
-      num % possible_factor != 0
-    end
+if num<0 or num==0 or num==1
+  return FALSE
+else 
+  (2..num-1).to.a_all? do |possible_factor|
+    num % possible_factor =! 0
   end
 end
-
-
+end
